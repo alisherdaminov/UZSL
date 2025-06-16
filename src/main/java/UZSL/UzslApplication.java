b79932abb9cb43d447c78e4b27af920cea878ab7
Alisher <101879695+Alisher19121994@users.noreply.github.com>
2025-06-16 17:17:48 +0500

    started UZSL pro

---
+++ src/main/java/UZSL/UzslApplication.java
@@ -0,0 +1,13 @@
+package UZSL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UzslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/UZSL/UzslApplicationTests.java
@@ -0,0 +1,13 @@
+package UZSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
